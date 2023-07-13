72077af29a47a42d11c4c79d29080879ed9f688d
kiliman <kiliman@gmail.com>
2023-07-13 09:57:45 -0400

    🐛 Fix external link handling
    Only check for basename if path is **not** a relative path

--- app/components/link.tsx
+++ app/components/link.tsx
@@ -3,13 +3,18 @@ import { useRootLoaderData } from '~/utils/hooks';
 
 // custom Link component that automatically reloads the document if the link is
 // external to the app or base path
-export function Link({ to, ...props }: LinkProps) {
+export function Link({ to, reloadDocument, ...props }: LinkProps) {
   const { basename } = useRootLoaderData();
+  const path = String(to);
+  const isExternal =
+    path.startsWith('http') ||
+    (path.startsWith('/') && !path.startsWith(basename));
+
   return (
     <RemixLink
       to={to}
       {...props}
-      reloadDocument={props.reloadDocument || !String(to).startsWith(basename)}
+      reloadDocument={reloadDocument || isExternal}
     />
   );
 }
